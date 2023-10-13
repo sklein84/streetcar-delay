1f7daabb0552858b2c5bd5a7e54520498969c9b9
Sebastian Klein <sebastian.a.klein@gmail.com>
2023-10-13 14:35:18 -0400

    display stop name on mouseover

--- delayDashboard/src/app/line-map/line-map.component.ts
+++ delayDashboard/src/app/line-map/line-map.component.ts
@@ -1,11 +1,16 @@
 import {
   Component,
   EventEmitter,
+  Inject,
   Input,
   OnChanges,
+  OnDestroy,
+  OnInit,
   Output,
+  Renderer2,
   SimpleChanges,
 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { LineService } from '../line-service.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
@@ -14,25 +19,56 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   templateUrl: './line-map.component.html',
   styleUrls: ['./line-map.component.css'],
 })
-export class LineMapComponent implements OnChanges {
+export class LineMapComponent implements OnInit, OnChanges, OnDestroy {
   @Input({ required: true }) line: string = '';
   selectedLine: HTMLElement | null = null;
   @Output() selectedLineEmitter = new EventEmitter<HTMLElement | null>();
+  hoveringStop: string = '';
+  listenerDeleters: (() => void)[] = [];
   svgMap: SafeHtml = '';
 
   constructor(
     private lineService: LineService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
   ) {}
+
+  ngOnInit(): void {
+    this.lineService.getMap(this.line).subscribe((map) => {
+      this.svgMap = this.sanitizer.bypassSecurityTrustHtml(map);
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['line'].previousValue != changes['line'].currentValue) {
-      this.lineService
-        .getMap(this.line)
-        .subscribe(
-          (img) => (this.svgMap = this.sanitizer.bypassSecurityTrustHtml(img))
-        );
+      this.lineService.getMap(this.line).subscribe((map) => {
+        this.svgMap = this.sanitizer.bypassSecurityTrustHtml(map);
+        setTimeout(() => this.setUpStopListeners(), 100);
+      });
     }
+  }
+
+  ngOnDestroy(): void {
+    this.cleanUpListeners();
+  }
+
+  private cleanUpListeners(): void {
+    this.listenerDeleters.forEach((f) => f());
+    this.listenerDeleters = [];
+  }
+
+  private setUpStopListeners(): void {
+    this.cleanUpListeners();
+
+    const circleElements = this.document.querySelectorAll('circle');
+    circleElements.forEach((circle) => {
+      this.listenerDeleters.push(
+        this.renderer.listen(circle, 'mouseenter', () => {
+          this.hoveringStop = circle.id.slice(5);
+        })
+      );
+    });
   }
 
   private resetSelectedLineStyle() {
